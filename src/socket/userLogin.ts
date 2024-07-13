45e8127f870bedb40583e39814f5fe5e6708f4dc
Leandro Gonçalves <leandrogoncalvespr@hotmail.com>
2024-07-13 13:14:55 -0300

    fix: warning calc

--- src/socket/userLogin.ts
+++ src/socket/userLogin.ts
@@ -1,41 +1,42 @@
 import { ISocket } from ".";
 import { prisma } from "..";
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
 const DAYS_TO_WARNING = 7;
 
 export const UserLogin = async (socket: ISocket, guid: string) => {
-    console.log("try to connect =>", guid)
+  console.log("try to connect =>", guid);
 
-    const user = await prisma.user.findUnique({
-        where: {
-            guid
-        }
-    })
+  const user = await prisma.user.findUnique({
+    where: {
+      guid,
+    },
+  });
 
-    console.log("user", user)
+  console.log("user", user);
 
-    if (!user) return;
-    socket.join(guid);
-    socket.data.guid = guid
+  if (!user) return;
+  socket.join(guid);
+  socket.data.guid = guid;
 
-    if (user.expirationDate < new Date()) {
-        socket.emit("expired")
-        return;
-    }
+  if (user.expirationDate < new Date()) {
+    socket.emit("expired");
+    return;
+  }
 
-    await prisma.user.updateMany({
-        where: {
-            guid,
-        },
-        data: {
-            isLogged: true
-        }
-    })
+  await prisma.user.updateMany({
+    where: {
+      guid,
+    },
+    data: {
+      isLogged: true,
+    },
+  });
 
-    if (dayjs(user.expirationDate).add(DAYS_TO_WARNING, "day").isAfter(new Date)) {
-
-        socket.emit("warning", dayjs(user.expirationDate).diff(new Date, "day"))
-        return;
-    }
-}
+  if (
+    dayjs(new Date()).add(DAYS_TO_WARNING, "day").isAfter(user.expirationDate)
+  ) {
+    socket.emit("warning", dayjs(user.expirationDate).diff(new Date(), "day"));
+    return;
+  }
+};
